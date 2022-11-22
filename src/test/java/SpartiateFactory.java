8ad99d3c177ff90edd0ed5965288b0f0e6c8a60f
HLetourneur <herve.letourneur@meritis.fr>
2022-11-22 01:25:25 +0100

    modification factory

--- src/test/java/SpartiateFactory.java
+++ src/test/java/SpartiateFactory.java
@@ -2,11 +2,17 @@ import com.meritis.sparte.people.Homoioi;
 import com.meritis.sparte.people.JeuneCitoyen;
 
 public class SpartiateFactory {
+    private static final String LEONIDAS = "Leonidas";
+
     public static Homoioi leonidas() {
-        return new Homoioi("Leonidas");
+        return new Homoioi(LEONIDAS);
     }
 
     public static JeuneCitoyen jeuneLeonidas() {
-        return new JeuneCitoyen("Leonidas", 20);
+        return new JeuneCitoyen(LEONIDAS, 20);
+    }
+
+    public static  JeuneCitoyen jeuneLeonidasAgeDe(int age) {
+        return new JeuneCitoyen(LEONIDAS, age);
     }
 }
